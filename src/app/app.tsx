f8c65ddd1f7fa2e3e3dec6c0f3464fb44bd8b66f
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-01 21:15:41 +0400

    add App

---
+++ src/app/app.tsx
@@ -0,0 +1,8 @@
+fetch('http://localhost:3001/users').then((res) => {
+    console.log(res)
+})
+
+export function App() {
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
