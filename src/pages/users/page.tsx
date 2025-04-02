489ac2bc2df1b93f17c740ee1d735a20de3e2f4f
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-02 20:11:16 +0400

    delete mx-auto, add disabled for imput and button

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,4 +1,4 @@
-import { Suspense, use, useState } from "react";
+import { Suspense, use, useState, useTransition } from "react";
 import { createUser, fetchUsers } from "../../Shared/api";
 
 type User = {
@@ -19,7 +19,7 @@ export const UsersPage = () => {
       <h1 className="text-3xl font-bold underline">Users</h1>
       <CreateUserForm refetchUsers={refetchUsers} />
       <Suspense fallback={<div>Loading...</div>}>
-        <UsersList usersPromise={usersPromise} />
+        <UsersList usersPromise={usersPromise} refetchUsers={refetchUsers} />
       </Suspense>
     </main>
   );
@@ -28,25 +28,35 @@ export const UsersPage = () => {
 export function CreateUserForm({ refetchUsers }: { refetchUsers: () => void }) {
   const [email, setEmail] = useState("");
 
+  const [isPending, startTransition] = useTransition(); //хук, кот. возвращает isPending и startTransition. это ф-ия перехода, долгие обновления. можно сделать асинхронные запросы
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    await createUser({
-      email,
-      id: crypto.randomUUID(),
+
+    startTransition(async () => {
+      await createUser({
+        email,
+        id: crypto.randomUUID(),
+      });
+      startTransition(() => {
+        refetchUsers();
+        setEmail("");
+      });
     });
-    refetchUsers();
-    setEmail("");
   };
+
   return (
     <form className="flex gap-2" onSubmit={handleSubmit}>
       <input
         className="border p-2 rounded"
         type="email"
         value={email}
+        disabled={isPending}
         onChange={(e) => setEmail(e.target.value)}
       />
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-300"
+        disabled={isPending}
         type="submit"
       >
         Add
@@ -55,18 +65,24 @@ export function CreateUserForm({ refetchUsers }: { refetchUsers: () => void }) {
   );
 }
 
-export function UsersList({ usersPromise }: { usersPromise: Promise<User[]> }) {
+export function UsersList({
+  usersPromise,
+  refetchUsers,
+}: {
+  usersPromise: Promise<User[]>;
+  refetchUsers: () => void;
+}) {
   const users = use(usersPromise); // new hook. основная задача прямо в рендере превратить usersPromise в user'ов. не м.б массива
   return (
     <div className="flex flex-col">
       {users.map((user) => (
-        <UserCard key={user.id} user={user} />
+        <UserCard key={user.id} user={user} refetchUsers={refetchUsers} />
       ))}
     </div>
   );
 }
 
-export function UserCard({ user }: { user: User }) {
+export function UserCard({ user }: { user: User; refetchUsers: () => void }) {
   return (
     <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2">
       {user.email}
