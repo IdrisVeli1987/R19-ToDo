ef33faff27c89b6ab30ad58f589e8a5ca1314632
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-07 19:19:29 +0400

    remove throw error

--- src/Shared/api.ts
+++ src/Shared/api.ts
@@ -11,8 +11,6 @@ export function fetchUsers() {
 }
 
 export function createUser(user: User) {
-  throw new Error("Not implemented");
-
   return fetch("http://localhost:3001/users", {
     method: "POST",
     headers: {
