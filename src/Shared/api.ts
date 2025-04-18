a436e42b3ba6303607e1c34c9ee13bc81cc6a1c6
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-18 15:49:07 +0400

    add todo-list api's

--- src/Shared/api.ts
+++ src/Shared/api.ts
@@ -3,7 +3,6 @@ export type User = {
   email: string;
 };
 
-
 export function fetchUsers() {
   return fetch("http://localhost:3001/users").then(
     (res) => res.json() as Promise<User[]>
@@ -23,5 +22,61 @@ export function createUser(user: User) {
 export function deleteUser(id: string) {
   return fetch(`http://localhost:3001/users/${id}`, {
     method: "Delete",
-  }).then((res) => res.json())
+  }).then((res) => res.json());
+}
+
+export type Task = {
+  id: string;
+  userId: string;
+  title: string;
+  done: string;
+  cretaedAt: string;
+};
+
+export function fetchTasks({
+  page = 1,
+  per_page = 10,
+  sort = { createdAt: "asc" },
+  filters,
+}: {
+  page?: number;
+  per_page?: number;
+  filters?: {
+    userId: string;
+  };
+  sort?: {
+    createdAt: "asc" | "desc";
+  };
+}) {
+  return fetch(
+    `http://localhost:3001/tasks?_page=${page}&_per_page=${per_page}&_sort=${
+      sort.createdAt === "asc" ? "createdAt" : "-createdAt"
+    }&userId=${filters?.userId}`
+  ).then((res) => res.json() as Promise<Task[]>);
+}
+
+export function createTask(task: Omit<Task, "id" | "createdAt">) {
+  return fetch("http://localhost:3001/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  }).then((res) => res.json());
+}
+
+export function updateTask(id: string, task: Partial<Task>) {
+  return fetch(`http://localhost:3001/tasks/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  }).then((res) => res.json());
+}
+
+export function deleteTask(id: string) {
+  return fetch(`http://localhost:3001/tasks/${id}`, {
+    method: "DELETE",
+  }).then((res) => res.json());
 }
