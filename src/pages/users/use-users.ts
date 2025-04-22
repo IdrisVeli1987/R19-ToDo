d7ce1b9616cecdfa3848850f6b389ab59aab0c6f
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-22 17:17:37 +0400

    add  defaultUsersPromise

--- src/pages/users/use-users.ts
+++ src/pages/users/use-users.ts
@@ -5,6 +5,7 @@ import { createUserAction, deleteUserAction } from "./actions";
 const defaultUsersPromise = fetchUsers();
 
 export function useUsers() {
+  const defaultUsersPromise = fetchUsers();
   const [usersPromise, setUsersPromise] = useState(defaultUsersPromise);
   const refetchUsers = () =>
     startTransition(() => setUsersPromise(fetchUsers()));
