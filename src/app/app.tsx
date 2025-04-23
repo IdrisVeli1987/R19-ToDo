c8eb6c0ebd432d4d80be83b5f7f989c7c905f14f
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-23 18:55:13 +0400

    fix code

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { TodoListPage } from "../pages/todo-list";
 import { UsersPage } from "../pages/users";
-import { UsersProvider } from "../pages/users/users-context";
+import { UsersProvider } from "../entities/user";
 
 export function App() {
   return (
