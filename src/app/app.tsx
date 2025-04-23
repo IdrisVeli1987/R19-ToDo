70cd31b70e779bcd081364504768393beab8cc7d
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-23 19:26:10 +0400

    delete UsersProvider

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { TodoListPage } from "../pages/todo-list";
 import { UsersPage } from "../pages/users";
-import { UsersProvider } from "../entities/user";
 
 export function App() {
   return (
-    <UsersProvider>
       <Routes>
         <Route path="/" element={<UsersPage />} />
         <Route path="/:userId/tasks" element={<TodoListPage />} />
       </Routes>
-    </UsersProvider>
   );
 }
 
