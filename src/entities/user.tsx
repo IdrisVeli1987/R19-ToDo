738b9c816b5a7ed8391d42083d9601f660d3db37
IdrisVeli1987 <idrisveliev@gmail.com>
2025-04-23 19:36:16 +0400

    return UserContextType, UsersContext, UsersProvider, useUsersGlobal

--- src/entities/user.tsx
+++ src/entities/user.tsx
@@ -1,14 +1,32 @@
-import { startTransition } from "react";
-import { create } from "zustand";
+import { createContext, startTransition, use, useState } from "react";
 import { fetchUsers, User } from "../Shared/api";
 
-type UserState = {
+type UserContextType = {
   usersPromise: Promise<User[]>;
   refetchUsers: () => void;
 };
 
-export const useUsersGlobal = create<UserState>((set) => ({
-  usersPromise: fetchUsers(),
-  refetchUsers: () =>
-    startTransition(() => set({ usersPromise: fetchUsers() })),
-}));
+const UsersContext = createContext<UserContextType | null>(null);
+
+const defaultUsersPromise = fetchUsers();
+
+export function UsersProvider({ children }: { children: React.ReactNode }) {
+  const [usersPromise, setUsersPromise] = useState(defaultUsersPromise);
+
+  const refetchUsers = () =>
+    startTransition(() => setUsersPromise(fetchUsers()));
+
+  return (
+    <UsersContext value={{ usersPromise, refetchUsers }}>
+      {children}
+    </UsersContext>
+  );
+}
+
+export function useUsersGlobal() {
+  const context = use(UsersContext);
+  if (!context) {
+    throw new Error("useUsers must be used within a UsersProvider");
+  }
+  return context;
+}
